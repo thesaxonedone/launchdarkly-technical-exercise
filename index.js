74327f8af314c2f9a8acdb0415310c7c7c9ed2cb
Grey <greyschober@gmail.com>
2021-09-09 10:04:21 -0700

    added index.js with basic implementation of feature flagging

---
+++ index.js
@@ -0,0 +1,35 @@
+//Connect to launch darkly
+var LaunchDarkly = require('launchdarkly-node-server-sdk');
+var ldclient = LaunchDarkly.init('sdk-d9ea449d-7652-431d-a8fa-9275875a8013');
+var user = {
+  key: 'user1',
+  custom: {
+    groups: 'beta_testers'
+  }
+};
+
+
+//Main execution:
+
+
+
+ldclient.once('ready', function() {
+
+  console.log('');
+  console.log('');
+  console.log('This text represents an old feature that already exists in our app');
+  console.log('');
+
+  ldclient.variation('greytest', user, false, function(err, showFeature) {
+    // console.log("SDK successfully connected! The value of greytest is " + showFeature + " for " + user.key)
+    if (showFeature === true) {
+      console.log('This text represents a new feature for which we are implementing feature flagging.  ' +
+        'This text should only appear if we are user Ruby Rod (user key of \'user1\'), due to my basic ' +
+        'implementation of targeting...');
+    }
+  });
+  ldclient.flush(function() {
+    ldclient.close();
+  });
+});
+
